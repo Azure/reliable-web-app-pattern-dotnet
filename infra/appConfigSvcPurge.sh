d7a649600d618894b3215ce81918c9a276d8bf0c
Ken Schlobohm <keschlob@microsoft.com>
2022-08-09 11:16:21 -0500

    manual purge

--- infra/appConfigSvcPurge.sh
+++ infra/appConfigSvcPurge.sh
@@ -27,6 +27,8 @@ echo -e "----------------------------------------------\n"
 echo -e "appcfgname=$appcfgname\n"
 echo -e "\n"
 
-az appconfig purge --name $appcfgname --yes 2> /dev/null
+#az appconfig purge --name $appcfgname --yes 2> /dev/null
+az resource delete -g relecloudresourcesnightly-rg -n azj5cfvdzwfr2-appconfig --resource-type "Microsoft.AppConfiguration/configurationStores" --verbose
+
 echo "Purged $appcfgname"  
 sleep 3 # give Azure some time to propagate this event
