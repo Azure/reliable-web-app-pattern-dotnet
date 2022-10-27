4163cf05c43ff20a787a22a8f918c3fa342d2de3
Ken Schlobohm <keschlob@microsoft.com>
2022-10-27 11:15:04 -0500

    adding kv purge for issue 1018

--- infra/appConfigSvcPurge.sh
+++ infra/appConfigSvcPurge.sh
@@ -35,5 +35,26 @@ if [[ ${#deletedAppConfigSvcName} -gt 0 ]]; then
   az appconfig purge --name $deletedAppConfigSvcName --yes
   echo "Purged $deletedAppConfigSvcName"  
 else
-   echo "Nothing to purge"
+   echo "No appconfig to purge"
 fi
+
+deletedKeyVaultSvcName=$(az keyvault list-deleted --query "[? properties.vaultId.contains(@,'$resourceGroupName')] | [0].name" -o tsv)
+currentRetryCount=0
+
+while [ ${#deletedKeyVaultSvcName} -ne 0 ]
+do
+  if [[ $currentRetryCount -gt 6 ]]; then
+    echo "FATAL ERROR: Tried to purge key vault too many times" 1>&2
+    exit 14
+  fi
+
+  if [[ ${#deletedKeyVaultSvcName} -gt 0 ]]; then
+    echo "Purging $deletedKeyVaultSvcName"
+    az keyvault purge --name $deletedKeyVaultSvcName
+  else
+    echo "Done purging key vaults"
+  fi
+
+  currentRetryCount=$((currentRetryCount + 1))
+  deletedKeyVaultSvcName=$(az keyvault list-deleted --query "[? properties.vaultId.contains(@,'$resourceGroupName')] | [0].name" -o tsv)
+done
