d16b4653b4560a6f30418228147d648d061a3a02
briandenicola <brian@briandenicola.com>
2023-02-01 18:13:27 +0000

    Merge devcontainer steps

--- .devcontainer/post-start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# this runs each time the container starts
-
-echo "$(date)    post-start start" >> ~/status
-
-azd config set auth.useAzCliAuth true
-
-echo "$(date)    post-start complete" >> ~/status
