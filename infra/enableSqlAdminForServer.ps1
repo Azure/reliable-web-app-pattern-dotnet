287c89140d852b7eeb8855d545676b8348d52e6d
Ken Schlobohm <keschlob@microsoft.com>
2023-02-01 15:17:54 -0600

    describe script separation #224

--- infra/enableSqlAdminForServer.ps1
+++ infra/enableSqlAdminForServer.ps1
@@ -1,6 +1,7 @@
 # This script is not intended to be run from a local environment.
-# This script is run by azd during devOps deployment. This script handles rolling back network changes
+# This script is run by azd during devOps deployment. This script handles rolling back auth changes
 # that would block the createSqlAcctForManagedIdentity.ps1 scripts from connecting when run as a deploymentScript
+# https://github.com/Azure/reliable-web-app-pattern-dotnet/issues/224
 
 # This script provides a workflow to automatically configure the deployed Azure resources and make it easier to get
 # started. It is not intended as part of a recommended best practice as we do not recommend deploying Azure SQL
