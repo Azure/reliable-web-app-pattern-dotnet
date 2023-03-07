1f4b4db7e568479064514e42d73d5536070346e2
Ken Schlobohm <keschlob@microsoft.com>
2023-03-07 16:24:18 -0600

    fix string index operations for bash

--- infra/localDevScripts/giveCurrentUserAccessToReadAppConfigSvc.sh
+++ infra/localDevScripts/giveCurrentUserAccessToReadAppConfigSvc.sh
@@ -52,4 +52,4 @@ fi
 appConfigDataReaderRole='516239f1-63e1-4d78-a4de-a74fb236a071'
 currentUserObjectId=$(az ad signed-in-user show --query "id")
 scopeId=$(az group show -n $resourceGroupName --query "id")
-az role assignment create --role $appConfigDataReaderRole --assignee ${currentUserObjectId:1:-2} --scope ${scopeId:1:-2}
+az role assignment create --role $appConfigDataReaderRole --assignee ${currentUserObjectId:1:-1} --scope ${scopeId:1:-1}
