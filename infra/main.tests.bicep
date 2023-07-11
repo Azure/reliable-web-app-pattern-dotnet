581dd915d28175352debb2fe9f480991df6b8c71
Rohan Cragg <rohan.cragg@qnrl.com>
2023-07-11 16:58:44 +0100

    Added config and script for running tests locally

---
+++ infra/main.tests.bicep
@@ -0,0 +1,19 @@
+﻿targetScope = 'subscription'
+
+module main 'main.bicep' = {
+  name: 'main'
+  params: {
+    location: 'westeurope'
+    secondaryAzureLocation: 'northeurope'
+    azureAdApiScopeFrontEnd: ''
+    azureAdClientIdForBackEnd: ''
+    azureAdClientIdForFrontEnd: ''
+    azureAdClientSecretForFrontEnd: ''
+    azureAdTenantId: ''
+    name: 'main'
+    principalId: 'x'
+    principalType: 'ServicePrincipal'
+    azureSqlPassword: ''
+    enableTelemetry: true
+  }
+}
---
+++ infra/main.tests.ps1
@@ -0,0 +1,22 @@
+﻿﻿####################################################################################################
+# Install prerequisites:
+####################################################################################################
+winget install -s winget -e --id "Microsoft.PowerShell"
+winget install -s winget -e --id "Microsoft.Bicep"
+
+Install-Module -Name 'Az' -Repository PSGallery -Force
+Install-Module -Name 'PSRule.Rules.Azure' -Repository PSGallery -Scope CurrentUser
+
+Set-Location .\infra\
+# report summary of all PsRule.Rules.Azure checks
+Invoke-PSRule -Format File -InputPath '*.tests.bicep' -Module 'PSRule.Rules.Azure' -Outcome Processed -As Summary
+
+# report detail of all PsRule.Rules.Azure checks
+Invoke-PSRule -Format File -InputPath '*.tests.bicep' -Module 'PSRule.Rules.Azure' -Outcome Fail, Error -As Detail
+
+# report detail of all PsRule.Rules.Azure checks with formatting
+Assert-PSRule -Format File -InputPath '*.tests.bicep' -Module 'PSRule.Rules.Azure' -Outcome Fail, Error
+
+# Note: Invoke-PSRule and Assert-PSRule are the same except for the output format
+# * Invoke-PSRule writes results as structured objects
+# * Assert-PSRule writes results as a formatted string
