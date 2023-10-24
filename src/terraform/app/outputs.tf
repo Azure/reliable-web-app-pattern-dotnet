d7a6c641741e3c8a44591cf1a4016f6d0b0f8f87
markti <markti@outlook.com>
2023-10-24 15:00:28 -0400

    admin password

--- src/terraform/app/outputs.tf
+++ src/terraform/app/outputs.tf
@@ -2,10 +2,9 @@ output "registry_endpoint" {
   value = azurerm_container_registry.main.login_server
 }
 output "registry_admin_username" {
-  value     = azurerm_container_registry.main.admin_username
-  sensitive = true
+  value = azurerm_container_registry.main.admin_username
 }
 output "registry_admin_password" {
-  value     = azurerm_container_registry.main.admin_username
+  value     = azurerm_container_registry.main.admin_password
   sensitive = true
 }
