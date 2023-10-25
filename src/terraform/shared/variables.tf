df5fca099d61d1a4efae285306c8651bf5d234e9
markti <markti@outlook.com>
2023-10-25 12:29:47 -0400

    fix stuff

---
+++ src/terraform/shared/variables.tf
@@ -0,0 +1,12 @@
+variable "application_name" {
+  type = string
+}
+variable "environment_name" {
+  type = string
+}
+variable "primary_region" {
+  type = string
+}
+variable "container_registry_pushers" {
+  type = list(string)
+}
