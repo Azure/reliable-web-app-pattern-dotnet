08136bd41ec1a4e4d46b09897c314cb67b0b1961
markti <markti@outlook.com>
2023-10-25 12:25:23 -0400

    update pipelines

---
+++ src/terraform/registry/container-registry.tf
@@ -0,0 +1,18 @@
+resource "azurerm_container_registry" "main" {
+  name                    = "acr${var.application_name}${var.environment_name}"
+  resource_group_name     = azurerm_resource_group.main.name
+  location                = azurerm_resource_group.main.location
+  sku                     = "Premium"
+  admin_enabled           = true
+  zone_redundancy_enabled = true
+}
+
+resource "azurerm_role_assignment" "acr_push" {
+
+  count = length(var.container_registry_pushers)
+
+  scope                = azurerm_container_registry.main.id
+  role_definition_name = "AcrPush"
+  principal_id         = var.container_registry_pushers[count.index]
+
+}
---
+++ src/terraform/registry/terraform.tfvars
@@ -0,0 +1,2 @@
+primary_region             = "westus2"
+container_registry_pushers = ["328976f7-25d0-4f66-8dfb-30608805ef95"]
