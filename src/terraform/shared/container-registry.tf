612e74762a16cbea0cd8dea6b5f8847f29019221
markti <markti@outlook.com>
2023-10-25 13:27:39 -0400

    remove hyphens

--- src/terraform/shared/container-registry.tf
+++ src/terraform/shared/container-registry.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "main" {
-  name                    = "acr${var.application_name}${var.environment_name}"
+  name                    = replace("acr${var.application_name}${var.environment_name}", "-", "")
   resource_group_name     = azurerm_resource_group.main.name
   location                = azurerm_resource_group.main.location
   sku                     = "Premium"
