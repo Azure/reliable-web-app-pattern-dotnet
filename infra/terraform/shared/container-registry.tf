19fd3776e3057f9be9b616b0904342ee31856370
markti <markti@outlook.com>
2023-10-29 11:46:36 -0400

    fix acr name

--- infra/terraform/shared/container-registry.tf
+++ infra/terraform/shared/container-registry.tf
@@ -1,5 +1,9 @@
+locals {
+  container_registry_name = replace("acr${var.application_name}${var.environment_name}", "-", "")
+}
+
 resource "azurerm_container_registry" "main" {
-  name                = "acr-${var.application_name}-${var.environment_name}"
+  name                = local.container_registry_name
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   sku                 = "Premium"
