df9388675bf97264dc1b2f46906aa54268a53745
markti <markti@outlook.com>
2023-11-01 17:44:56 -0400

    use mock image service

--- src/dotnet/Relecloud.Web.Api/Startup.cs
+++ src/dotnet/Relecloud.Web.Api/Startup.cs
@@ -66,7 +66,7 @@ namespace Relecloud.Web.Api
             else
             {
                 services.AddScoped<ITicketManagementService, TicketManagementService>();
-                services.AddScoped<ITicketRenderingService, TicketRenderingService>();
+                services.AddScoped<ITicketRenderingService, MockTicketRenderingService>();
             }
         }
 
