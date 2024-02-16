079ea271e186ce0c567bd865f1a04798ebbdb88d
Ken Schlobohm <keschlob@microsoft.com>
2024-02-16 01:06:28 +0000

    format web_uri output

--- infra/scripts/postdeploy/show-webapp-uri.ps1
+++ infra/scripts/postdeploy/show-webapp-uri.ps1
@@ -2,5 +2,13 @@
 # The AZD deploy command shows the links to the azurewebsites.net resources
 # We block access to these resources and instead want to show the Azure Front Door URL
 
+# Prompt formatting features
+
+$defaultColor = if ($Host.UI.SupportsVirtualTerminal) { "`e[0m" } else { "" }
+$highlightColor = if ($Host.UI.SupportsVirtualTerminal) { "`e[36m" } else { "" }
+
+# End of Prompt formatting features
+
 Write-Host "Use this URI to access the web app:"
-Write-Host (azd env get-values --output json | ConvertFrom-Json).WEB_URI
+$azureFrontDoorUri=(azd env get-values --output json | ConvertFrom-Json).WEB_URI
+Write-Host "`t$($highlightColor)$azureFrontDoorUri$($defaultColor)"
--- infra/scripts/postdeploy/show-webapp-uri.sh
+++ infra/scripts/postdeploy/show-webapp-uri.sh
@@ -3,5 +3,4 @@
 # The AZD deploy command shows the links to the azurewebsites.net resources
 # We block access to these resources and instead want to show the Azure Front Door URL
 
-echo "Use this URI to access the web app:"
-echo $(azd env get-values --output json | jq -r '.WEB_URI')
+pwsh ./infra/scripts/postdeploy/show-webapp-uri.ps1
