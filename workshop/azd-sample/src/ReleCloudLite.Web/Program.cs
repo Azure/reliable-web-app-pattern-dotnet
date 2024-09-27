40f4c8e5c6bb905d514f7156738b785079b6d947
Pablo Lopes <pablolopes@microsoft.com>
2024-09-27 21:36:52 +0000

    Deleting unecessary pages and services from the sample

--- workshop/azd-sample/src/ReleCloudLite.Web/Program.cs
+++ workshop/azd-sample/src/ReleCloudLite.Web/Program.cs
@@ -9,7 +9,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<TicketService>();
 var azureCredential = builder.GetAzureTokenCredential();
 
